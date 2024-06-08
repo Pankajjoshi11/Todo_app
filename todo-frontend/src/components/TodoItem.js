f4cc740e0c9da1b664f180a46a9a8acf3a448f27
Pankajjoshi11 <127141403+Pankajjoshi11@users.noreply.github.com>
2024-06-08 20:19:13 +0530

    Update TodoItem.js

--- todo-frontend/src/components/TodoItem.js
+++ todo-frontend/src/components/TodoItem.js
@@ -34,21 +34,21 @@ const TodoItem = ({ todo }) => {
   };
 
   return (
-    <div className="bg-gray-600 shadow-md px-4 py-6 md:px-8 md:py-8 flex flex-col md:flex-row items-center my-2 mx-40">
-      <div className="md:mr-4 md:w-2/3">
-        <h3 className="text-orange-500 text-3xl font-bold mb-2">{todo.title}</h3>
+    <div className="bg-gray-600 shadow-md p-4 md:p-8 flex flex-col md:flex-row items-center my-2 mx-4 md:mx-40">
+      <div className="flex-grow md:mr-4">
+        <h3 className="text-orange-500 text-2xl md:text-3xl font-bold mb-2">{todo.title}</h3>
         <p className="text-white text-base">{todo.description}</p>
       </div>
-      <div className="flex justify-center items-center mt-4 md:mt-0 md:w-1/3">
+      <div className="flex justify-center items-center mt-4 md:mt-0 space-x-2 md:space-x-4">
         <button
           onClick={handleEdit}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2 md:mr-0 md:ml-2"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         >
           Edit
         </button>
         <button
           onClick={handleDelete}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mr-2 md:mr-0 md:ml-2"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
         >
           Delete
         </button>
@@ -56,7 +56,7 @@ const TodoItem = ({ todo }) => {
           onClick={handleComplete}
           className={`${
             todo.completed ? 'bg-green-500' : 'bg-gray-500 hover:bg-green-700'
-          } text-white font-bold py-2 px-4 rounded-full md:mr-0 md:ml-2`}
+          } text-white font-bold py-2 px-4 rounded-full`}
           disabled={todo.completed}
         >
           {todo.completed ? 'Completed' : 'Mark as Completed'}
